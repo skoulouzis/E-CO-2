ec868b85c65d198d3c3310a9d286f9ca42ebccde
skoulouzis <S.Koulouzis@uva.nl>
2016-07-01 11:20:58 +0200

    Dumy class

---
+++ Traning/src/main/java/eu/edisonproject/traning/context/corpus/Dumy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.edisonproject.traning.context.corpus;
+
+/**
+ *
+ * @author 
+ */
+public class Dumy {
+    
+}
