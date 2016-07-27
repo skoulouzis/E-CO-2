f415d55a49d99636434229b72fe1a58e05958168
skoulouzis <S.Koulouzis@uva.nl>
2016-07-27 16:19:40 +0300

    script for wsd

---
+++ scripts/wsd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+JAR_PATH=$HOME/workspace/E-CO-2/Traning/target/Traning-1.0-SNAPSHOT-jar-with-dependencies.jar
+DICTIONARY_ALL=$HOME/workspace/E-CO-2/etc/dictionaryAll.csv 
+STOPWORDS=$HOME/workspace/E-CO-2/etc/stopwords.csv
+MODEL_PATH=$HOME/workspace/E-CO-2/etc/model
+PROPS_FILE=$HOME/workspace/E-CO-2/etc/configure.properties
+
+TRAIN_DOC_PATHS=()
+CATEGORIES_FOLDER=$1
+for i in $(ls -d $CATEGORIES_FOLDER/*)
+do 
+  TRAIN_DOC_PATHS+=($i)
+done
+
+for i in "${TRAIN_DOC_PATHS[@]}"
+do
+  for f in $i/*.csv
+  do
+    base=`basename $i`
+    echo $base
+
+    echo "screen -dmSL $base java -Dstop.words.file=$STOPWORDS -Ditemset.file=$DICTIONARY_ALL -Dmodel.path=$MODEL_PATH -Dnum.of.terms=250 -Doffset.terms=1 -jar $JAR_PATH -op w -i $f -o $i/$base.avro -p $PROPS_FILE"
+    
+    echo "screen -dmSL $base java -Dstop.words.file=$STOPWORDS -Ditemset.file=$DICTIONARY_ALL -Dmodel.path=$MODEL_PATH -Dnum.of.terms=250 -Doffset.terms=250 -jar $JAR_PATH -op w -i $f -o $i/$base.avro -p $PROPS_FILE"
+    
+    echo "screen -dmSL $base java -Dstop.words.file=$STOPWORDS -Ditemset.file=$DICTIONARY_ALL -Dmodel.path=$MODEL_PATH -Dnum.of.terms=250 -Doffset.terms=500 -jar $JAR_PATH -op w -i $f -o $i/$base.avro -p $PROPS_FILE"
+    
+    echo "screen -dmSL $base java -Dstop.words.file=$STOPWORDS -Ditemset.file=$DICTIONARY_ALL -Dmodel.path=$MODEL_PATH -Dnum.of.terms=250 -Doffset.terms=750 -jar $JAR_PATH -op w -i $f -o $i/$base.avro -p $PROPS_FILE"
+        
+  done
+done
