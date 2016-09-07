be7f3447b20f01fdb371219998e7d5b37baaa092
skoulouzis <S.Koulouzis@uva.nl>
2016-09-07 20:40:51 +0200

    sort term

--- scripts/termExtraction.sh
+++ scripts/termExtraction.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 JAR_PATH=$HOME/workspace/E-CO-2/Traning/target/Traning-1.0-SNAPSHOT-jar-with-dependencies.jar
-DICTIONARY_ALL=$HOME/workspace/E-CO-2/etc/dictionaryAll.csv 
+DICTIONARY_ALL=$HOME/workspace/E-CO-2/etc/dictionaryAll.csv
 STOPWORDS=$HOME/workspace/E-CO-2/etc/stopwords.csv
 MODEL_PATH=$HOME/workspace/E-CO-2/etc/model
 PROPS_FILE=$HOME/workspace/E-CO-2/etc/configure.properties
@@ -9,14 +9,34 @@ TAGGER_FILE=$HOME/workspace/E-CO-2/etc/model/stanford/english-left3words-distsim
 TRAIN_DOC_PATHS=()
 CATEGORIES_FOLDER=$1
 for i in $(ls -d $CATEGORIES_FOLDER/*)
-do 
+do
   TRAIN_DOC_PATHS+=($i)
 done
 
+
+
+preserve -list |grep $USER | awk '{print $1}' > reservations
+
+
+while read p; do
+  res+=($p)
+done < reservations
+
+index=0
+resLen=${#res[@]}
 for i in "${TRAIN_DOC_PATHS[@]}"
 do
   for f in $i/*.txt
   do
-    java -Dstop.words.file=$STOPWORDS -Ditemset.file=$DICTIONARY_ALL -Dmodel.path=$MODEL_PATH -Dtagger.file=$TAGGER_FILE -jar $JAR_PATH -op x -i $f -o $i/terms.csv -p $PROPS_FILE
+        index=$((index+1))
+        base=`basename $i`
+        if [ "$index" -ge "$resLen" ];
+        then
+                index=0
+        fi
+        #echo index $index
+        #echo res $res
+        #echo len $resLen
+        echo "screen -dmSL $base prun -reserve ${res[$index]} -Dstop.words.file=$STOPWORDS -Ditemset.file=$DICTIONARY_ALL -Dmodel.path=$MODEL_PATH -Dtagger.file=$TAGGER_FILE -jar $JAR_PATH -op x -i $f -o $i/terms.csv -p $PROPS_FILE"
   done
 done
