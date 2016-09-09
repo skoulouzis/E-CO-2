6945be98db2c5b8c05f0a08cc322816d9c693cc9
skoulouzis <S.Koulouzis@uva.nl>
2016-09-09 19:50:51 +0200

    fix script

--- scripts/termExtraction.sh
+++ scripts/termExtraction.sh
@@ -37,6 +37,6 @@ do
         #echo index $index
         #echo res $res
         #echo len $resLen
-        echo "screen -dmSL $base prun -reserve ${res[$index]} -Dstop.words.file=$STOPWORDS -Ditemset.file=$DICTIONARY_ALL -Dmodel.path=$MODEL_PATH -Dtagger.file=$TAGGER_FILE -jar $JAR_PATH -op x -i $i -o $i/terms.csv -p $PROPS_FILE"
+        echo "screen -dmSL $base prun -reserve ${res[$index]} -np 1 java -Dstop.words.file=$STOPWORDS -Ditemset.file=$DICTIONARY_ALL -Dmodel.path=$MODEL_PATH -Dtagger.file=$TAGGER_FILE -jar $JAR_PATH -op x -i $i -o $i/terms.csv -p $PROPS_FILE"
   #done
 done
