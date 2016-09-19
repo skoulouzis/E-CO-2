76ab2dfda2c252d4bc7400b00480cb4f4b24415c
skoulouzis <S.Koulouzis@uva.nl>
2016-09-19 19:04:00 +0200

    comment out code for testing

--- Classification/src/main/java/eu/edisonproject/classification/main/BatchMain.java
+++ Classification/src/main/java/eu/edisonproject/classification/main/BatchMain.java
@@ -33,7 +33,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
