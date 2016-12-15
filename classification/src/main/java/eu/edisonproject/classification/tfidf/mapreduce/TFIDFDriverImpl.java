dd18cd1ce016fb30dd05ee820ead7737d2780829
skoulouzis <S.Koulouzis@uva.nl>
2016-12-15 13:20:37 +0100

    works with local

--- classification/src/main/java/eu/edisonproject/classification/tfidf/mapreduce/TFIDFDriverImpl.java
+++ classification/src/main/java/eu/edisonproject/classification/tfidf/mapreduce/TFIDFDriverImpl.java
@@ -38,31 +38,13 @@ import org.apache.hadoop.util.ToolRunner;
  */
 public class TFIDFDriverImpl {
 
-  //where to read the frequent itemset
   public static String INPUT_ITEMSET;
-  //where to put the data in hdfs when MapReduce#1 will finish
-//  public static String OUTPUT_PATH1 = System.currentTimeMillis() + "-TFIDFDriverImpl-1-word-freq";
-  // where to read the data for the MapReduce#2
-//  public static String INPUT_PATH2 = OUTPUT_PATH1;
-  // where to put the data in hdfs when the MapReduce#2 will finish
-//  public static String OUTPUT_PATH2 = System.currentTimeMillis() + "-TFIDFDriverImpl-2-word-counts";
-  // where to read the data for the MapReduce#3
-//  public static String INPUT_PATH3 = OUTPUT_PATH2;
-// where to put the data in hdfs when the MapReduce#3 will finish
-//  public static String OUTPUT_PATH3 = System.currentTimeMillis() + "-TFIDFDriverImpl-3-tf-idf";
 
-  // where to read the data for the MapReduce#4.
-//  public static String INPUT_PATH4 = OUTPUT_PATH3;
-  // where to put the data in hdfs when the MapReduce# will finish
-//  public static String OUTPUT_PATH4 = System.currentTimeMillis() + "-TFIDFDriverImpl-4-distances";
-  // where to put the csv with the tfidf
   public static String COMPETENCES_PATH;
 
-//    private String finalOutputPath;
   public static String NUM_OF_LINES;
   public static String STOPWORDS_PATH = ".." + File.separator + "etc" + File.separator + "stopwords.csv";
   public String OUT;
-//  public String AVRO_FILE = System.currentTimeMillis() + "-TFIDFDriverImpl-avro";
 
   public TFIDFDriverImpl() {
   }
@@ -99,48 +81,48 @@ public class TFIDFDriverImpl {
         String[] args1 = {INPUT_ITEMSET, OUTPUT_PATH1, inputPath, STOPWORDS_PATH, NUM_OF_LINES};
         ToolRunner.run(new TermWordFrequency(), args1);
       }
-//      String OUTPUT_PATH2 = System.currentTimeMillis() + "_" + UUID.randomUUID() + "-TFIDFDriverImpl-2-word-counts";;
-//      String[] args2 = {OUTPUT_PATH1, OUTPUT_PATH2};
-//      ToolRunner.run(new WordCountsForDocsDriver(), args2);
-//
-//      File docs = new File(inputPath);
-//
-//      File[] files = docs.listFiles(new FilenameFilter() {
-//        @Override
-//        public boolean accept(File dir, String name) {
-//          return name.toLowerCase().endsWith(".txt");
-//        }
-//      });
-//      Logger.getLogger(TFIDFDriverImpl.class.getName()).log(Level.INFO, "docs:{0}", docs.getAbsolutePath());
-//      int numberOfDocuments = files.length;
-//      String OUTPUT_PATH3 = System.currentTimeMillis() + "_" + UUID.randomUUID() + "-TFIDFDriverImpl-3-tf-idf";
-//      String[] args3 = {OUTPUT_PATH2, OUTPUT_PATH3, String.valueOf(numberOfDocuments)};
-//      ToolRunner.run(new WordsInCorpusTFIDFDriver(), args3);
-//
-//      StringBuilder fileNames = new StringBuilder();
-//      String prefix = "";
-//      for (File name : files) {
-//        if (name.isFile() && FilenameUtils.getExtension(name.getName()).endsWith("txt")) {
-//          fileNames.append(prefix);
-//          prefix = ",";
-//          fileNames.append(FilenameUtils.removeExtension(name.getName()).replaceAll("_", ""));
-//        }
-//      }
-//      String OUTPUT_PATH4 = System.currentTimeMillis() + "_" + UUID.randomUUID() + "-TFIDFDriverImpl-4-distances";
-//      String[] args4 = {OUTPUT_PATH3, OUTPUT_PATH4, COMPETENCES_PATH, fileNames.toString()};
-//      Logger.getLogger(TFIDFDriverImpl.class.getName()).log(Level.INFO, "args4:{0}", Arrays.toString(args4));
-//      ToolRunner.run(new CompetencesDistanceDriver(), args4);
-//
-//      Configuration conf = new Configuration();
-//      FileSystem fs = FileSystem.get(conf);
-//      Path hdfsRes = new Path(OUTPUT_PATH4);
-//      FileStatus[] results = fs.listStatus(hdfsRes);
-//      for (FileStatus s : results) {
-//        Path dest = new Path(OUT + "/" + s.getPath().getName());
-//        Logger.getLogger(TFIDFDriverImpl.class.getName()).log(Level.INFO, "Copy: {0} to: {1}", new Object[]{s.getPath(), dest});
-//        fs.copyToLocalFile(s.getPath(), dest);
-//      }
-//      fs.delete(hdfsRes, true);
+      String OUTPUT_PATH2 = System.currentTimeMillis() + "_" + UUID.randomUUID() + "-TFIDFDriverImpl-2-word-counts";;
+      String[] args2 = {OUTPUT_PATH1, OUTPUT_PATH2};
+      ToolRunner.run(new WordCountsForDocsDriver(), args2);
+
+      File docs = new File(inputPath);
+
+      File[] files = docs.listFiles(new FilenameFilter() {
+        @Override
+        public boolean accept(File dir, String name) {
+          return name.toLowerCase().endsWith(".txt");
+        }
+      });
+      Logger.getLogger(TFIDFDriverImpl.class.getName()).log(Level.INFO, "docs:{0}", docs.getAbsolutePath());
+      int numberOfDocuments = files.length;
+      String OUTPUT_PATH3 = System.currentTimeMillis() + "_" + UUID.randomUUID() + "-TFIDFDriverImpl-3-tf-idf";
+      String[] args3 = {OUTPUT_PATH2, OUTPUT_PATH3, String.valueOf(numberOfDocuments)};
+      ToolRunner.run(new WordsInCorpusTFIDFDriver(), args3);
+
+      StringBuilder fileNames = new StringBuilder();
+      String prefix = "";
+      for (File name : files) {
+        if (name.isFile() && FilenameUtils.getExtension(name.getName()).endsWith("txt")) {
+          fileNames.append(prefix);
+          prefix = ",";
+          fileNames.append(FilenameUtils.removeExtension(name.getName()).replaceAll("_", ""));
+        }
+      }
+      String OUTPUT_PATH4 = System.currentTimeMillis() + "_" + UUID.randomUUID() + "-TFIDFDriverImpl-4-distances";
+      String[] args4 = {OUTPUT_PATH3, OUTPUT_PATH4, COMPETENCES_PATH, fileNames.toString()};
+      Logger.getLogger(TFIDFDriverImpl.class.getName()).log(Level.INFO, "args4:{0}", Arrays.toString(args4));
+      ToolRunner.run(new CompetencesDistanceDriver(), args4);
+
+      Configuration conf = new Configuration();
+      FileSystem fs = FileSystem.get(conf);
+      Path hdfsRes = new Path(OUTPUT_PATH4);
+      FileStatus[] results = fs.listStatus(hdfsRes);
+      for (FileStatus s : results) {
+        Path dest = new Path(OUT + "/" + s.getPath().getName());
+        Logger.getLogger(TFIDFDriverImpl.class.getName()).log(Level.INFO, "Copy: {0} to: {1}", new Object[]{s.getPath(), dest});
+        fs.copyToLocalFile(s.getPath(), dest);
+      }
+      fs.delete(hdfsRes, true);
 
     } catch (Exception ex) {
       Logger.getLogger(TFIDFDriverImpl.class.getName()).log(Level.SEVERE, null, ex);
--- rest/src/main/java/eu/edisonproject/rest/ECO2Server.java
+++ rest/src/main/java/eu/edisonproject/rest/ECO2Server.java
@@ -6,6 +6,7 @@
 
 package eu.edisonproject.rest;
 
+import com.sun.jersey.spi.container.servlet.ServletContainer;
 import eu.edisonproject.utility.file.ConfigHelper;
 import eu.edisonproject.utility.file.MyProperties;
 import java.io.IOException;
@@ -14,7 +15,7 @@ import java.util.logging.Logger;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.server.ResourceConfig;
 
 /**
  *
@@ -51,31 +52,39 @@ public class ECO2Server {
     } catch (Exception ex) {
       Logger.getLogger(ECO2Server.class.getName()).log(Level.SEVERE, null, ex);
     } finally {
-      if (server != null) {
+      if (server != null && !server.isStopped()) {
         server.destroy();
       }
-
     }
   }
 
   private static Server startServer(String[] args) throws IOException {
-    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-    context.setContextPath("/");
     MyProperties props = null;
     Integer port = 9999;
     if (args != null) {
       props = ConfigHelper.getProperties(args[0]);
       port = Integer.valueOf(props.getProperty("e-co-2.server.port", "9999"));
     }
+    ResourceConfig config = new ResourceConfig();
+    config.packages("eu.edisonproject.rest");
+    ServletHolder servlet = new ServletHolder(new ServletContainer(config));
 
-    Server jettyServer = new Server(port);
-    jettyServer.setHandler(context);
+    Server server = new Server(port);
+    ServletContextHandler context = new ServletContextHandler(server, "/*");
+    context.addServlet(servlet, "/*");
 
-    ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
-    jerseyServlet.setInitOrder(0);
-
-    jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "eu.edisonproject.rest");
-    return jettyServer;
+//    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+//    context.setContextPath("/");
+//
+//    Server jettyServer = new Server(port);
+//    jettyServer.setHandler(context);
+//
+//    ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
+//    jerseyServlet.setInitOrder(0);
+//
+//    jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "eu.edisonproject.rest");
+//    return jettyServer;
+    return server;
   }
 
   private static Thread startTaskWatcher(String dir) throws IOException, InterruptedException {
