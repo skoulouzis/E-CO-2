9d0fd02a7620b2079387959a61d4d0117957e2b2
skoulouzis <skoulouzis@users.noreply.github.com>
2018-07-17 15:20:28 +0300

    Update add_jobs.sh

--- scripts/add_jobs.sh
+++ scripts/add_jobs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-# 
-# id=0
-# mkdir /tmp/jobs
-# for f in $1/*.json; do
-#     id=$((id+1))
-#     cont=`jq -r .description $f`
-#     jq --null-input --compact-output --arg cont "$cont" --arg id "$id" '{"id":$id,"contents": $cont}' > /tmp/jobs/$id.json
-# done
+
+id=0
+mkdir /tmp/jobs
+for f in $1/*.json; do
+    id=$((id+1))
+    cont=`jq -r .description $f`
+    jq --null-input --compact-output --arg cont "$cont" --arg id "$id" '{"id":$id,"contents": $cont}' > /tmp/jobs/$id.json
+done
 
 
 
